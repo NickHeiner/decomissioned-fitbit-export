73af6ff2f398f68fe5b985cf038e3b30e9a46064
Nick Heiner <nick.heiner@opower.com>
2014-06-08 14:09:24 -0400

    Tweaking to make it work on azure.

--- app/fitbit-export.js
+++ app/fitbit-export.js
@@ -2,22 +2,27 @@
 
 var express = require('express'),
     app = express(),
+    path = require('path'),
+    morgan = require('morgan'),
     getConfig = require('./get-config'),
     auth = require('./auth'),
     _ = require('lodash'),
     server;
 
+app.use(morgan());
+
 app.get('/', function(req, res){
-    res.render('../views/index.ejs', {
+    res.render('index.ejs', {
         loggedIn: req.session && req.session.passport && req.session.passport.user.id
     });
 });
 
+// https://github.com/visionmedia/express/pull/2165
+app.set('views', path.join(__dirname, '..', 'views'));
 app.set('view engine', require('ejs'));
 
 auth(app);
 
 server = app.listen(getConfig(app).port, function() {
-
-    console.log('Listening on port %d', server.address().port);
+    console.log(require('../package').name + ' express app listening at', server.address());
 });
