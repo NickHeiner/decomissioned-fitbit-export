262087ad6ece73926e8310c7b537228b993bcb0b
Nick Heiner <nick.heiner@opower.com>
2014-06-08 17:14:44 -0400

    trying to properly combine the responses

--- app/get-time-series.js
+++ app/get-time-series.js
@@ -3,6 +3,7 @@
 var _ = require('lodash'),
     q = require('q'),
     getTimeSeriesForResource = require('./get-time-series-for-resource'),
+    combineFitbitResponses = require('./combine-fitbit-responses'),
     resources = Object.freeze({
         activities: [
             'distance',
@@ -19,7 +20,7 @@ function getTimeSeries(app, user) {
     return q.all(_.map(resources, function(subCategory, category) {
         return getTimeSeriesForResource(app, user, '2013-12-01', '7d', category, subCategory);
     })).then(function(timeSeriesPerResource) {
-        return _.merge.apply(_, [{}].concat(timeSeriesPerResource));
+        return combineFitbitResponses(timeSeriesPerResource);
     });
 }
 
