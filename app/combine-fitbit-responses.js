0f2b5c1c8fb0e04bb729d7c7bea031bca28b8c65
Nick Heiner <nick.heiner@opower.com>
2014-06-08 17:08:34 -0400

    adding another test - does not yet pass

---
+++ app/combine-fitbit-responses.js
@@ -0,0 +1,6 @@
+'use strict';
+
+function combineFitbitResponses(responses) {
+}
+
+module.exports = combineFitbitResponses;
---
+++ test/unit/combine-fitbit-responses.js
@@ -0,0 +1,54 @@
+'use strict';
+
+var expect = require('chai').expect,
+    combineFitbitResponses = require('../../app/combine-fitbit-responses');
+
+describe('combine-fitbit-responses', function() {
+
+    it('combines the responses', function() {
+        var responses = [
+            {
+                dateTime: '2013-11-25',
+                'activities-distance': 2.419
+            },
+            {
+                dateTime: '2013-11-26',
+                'activities-distance': 4.83
+            },
+            {
+                dateTime: '2013-11-25',
+                'sleep-timeInBed': 480
+            },
+            {
+                dateTime: '2013-12-01',
+                'body-weight': 152
+            },
+            {
+                dateTime: '2013-12-01',
+                'sleep-timeInBed': 212
+            }
+        ]
+
+        expect(combineFitbitResponses(responses)).to.deep.equal([
+            {
+                dateTime: '2013-11-25',
+                'activities-distance': 2.419,
+                'sleep-timeInBed': 480
+            },
+            {
+                dateTime: '2013-11-26',
+                'activities-distance': 4.83
+            },
+            {
+                dateTime: '2013-12-01',
+                'body-weight': 152,
+                'sleep-timeInBed': 212
+            },
+            {
+                dateTime: '2013-11-26',
+                'activities-distance': 4.83
+            }
+        ]);
+    });
+
+});
