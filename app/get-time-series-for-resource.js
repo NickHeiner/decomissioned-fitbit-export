36d52f0058627428cfe3585f2a7e037a23160034
Nick Heiner <nick.heiner@opower.com>
2014-06-08 16:30:11 -0400

    refactoring getting data

---
+++ app/get-time-series-for-resource.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var q = require('q'),
+    getConfig = require('./get-config'),
+    url = require('url'),
+    _ = require('lodash'),
+    OAuth = require('oauth');
+
+function getTimeSeries(app, user, baseDate, period, resourceCategory, resourceSubcategory) {
+
+    var config = getConfig(app),
+        oauth = new OAuth.OAuth(
+            'https://api.fitbit.com/oauth/request_token',
+            'https://api.fitbit.com/oauth/access_token',
+            config.fitbitClientKey,
+            config.fitbitClientSecret,
+            '1.0',
+            null,
+            'HMAC-SHA1'
+        ),
+        deferred = q.defer(),
+        requestUrl = url.format({
+            protocol: 'http',
+            hostname: 'api.fitbit.com',
+            pathname: _.template('/1/user/-/<%= resourceCategory %>/<%= resourceSubcategory %>/date/<%= baseDate %>/<% period %>.json', {
+                resourceCategory: resourceCategory,
+                resourceSubcategory: resourceSubcategory,
+                baseDate: baseDate,
+                period: period
+            })
+        });
+
+    // I wanted to use q.ninvoke here but it didn't work for whatever reason.
+    oauth.get(
+        requestUrl,
+        user.token,
+        user.tokenSecret,
+        function(err, data) {
+            var jsonData,
+                responseKey = [resourceCategory, resourceSubcategory].join('-');
+
+            if (err) {
+                deferred.reject(err);
+            }
+
+            jsonData = JSON.parse(data);
+
+
+            deferred.resolve(jsonData[responseKey]);
+        }
+    );
+
+    return deferred.promise;
+}
+
+module.exports = getTimeSeries;
--- app/get-time-series.js
+++ app/get-time-series.js
@@ -1,42 +1,9 @@
 'use strict';
 
-var q = require('q'),
-    getConfig = require('./get-config'),
-    OAuth = require('oauth');
+var getTimeSeriesForResource = require('./get-time-series-for-resource');
 
 function getTimeSeries(app, user) {
-
-    var config = getConfig(app),
-        oauth = new OAuth.OAuth(
-            'https://api.fitbit.com/oauth/request_token',
-            'https://api.fitbit.com/oauth/access_token',
-            config.fitbitClientKey,
-            config.fitbitClientSecret,
-            '1.0',
-            null,
-            'HMAC-SHA1'
-        ),
-        deferred = q.defer();
-
-    // I wanted to use q.ninvoke here but it didn't work for whatever reason.
-    oauth.get(
-        'http://api.fitbit.com/1/user/-/activities/distance/date/2013-12-01/7d.json',
-        user.token,
-        user.tokenSecret,
-        function(err, data) {
-            var jsonData;
-
-            if (err) {
-                deferred.reject(err);
-            }
-
-            jsonData = JSON.parse(data);
-
-            deferred.resolve(jsonData['activities-distance']);
-        }
-    );
-
-    return deferred.promise;
+    return getTimeSeriesForResource(app, user, '2013-12-01', '7d', 'activites', 'distance');
 }
 
 module.exports = getTimeSeries;
