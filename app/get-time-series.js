cea04742707cf71b3332872cd09505cb226fe743
Nick Heiner <nick.heiner@opower.com>
2014-06-08 16:00:10 -0400

    try using real data

--- app/get-time-series.js
+++ app/get-time-series.js
@@ -1,14 +1,33 @@
 'use strict';
 
-var q = require('q');
+var q = require('q'),
+    getConfig = require('./get-config'),
+    OAuth = require('oauth');
 
-function getTimeSeries() {
-    return q([
-        {
-            "dateTime": "2013-11-25",
-            "distance": "2.41932"
-        }
-    ])
+function getTimeSeries(app, user) {
+
+    var config = getConfig(app),
+        oauth = new OAuth.OAuth(
+            'https://api.fitbit.com/oauth/request_token',
+            'https://api.fitbit.com/oauth/access_token',
+            config.fitbitClientKey,
+            config.fitbitClientSecret,
+            '1.0',
+            null,
+            'HMAC-SHA1'
+        );
+
+    return q.ninvoke(
+        oauth,
+        'post',
+        'http://api.fitbit.com/1/user/-/activities/distance/date/2013-12-01/7d.json',
+        user.token,
+        user.tokenSecret,
+        null,
+        null
+    ).then(function(data) {
+        return data['activities-distance'];
+    });
 }
 
 module.exports = getTimeSeries;
