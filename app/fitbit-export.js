eeafeceec2911a1f8f221a749a039b2656b57702
Nick Heiner <nick.heiner@opower.com>
2014-06-08 13:53:07 -0400

    woo convention

--- app/fitbit-export.js
+++ app/fitbit-export.js
@@ -4,12 +4,11 @@ var express = require('express'),
     app = express(),
     getConfig = require('./get-config'),
     auth = require('./auth'),
-    path = require('path'),
     _ = require('lodash'),
     server;
 
 app.get('/', function(req, res){
-    res.render(path.join('..', 'views', 'index.ejs'), {
+    res.render('index.ejs', {
         loggedIn: req.session && req.session.passport && req.session.passport.user.id
     });
 });
@@ -19,5 +18,5 @@ app.set('view engine', require('ejs'));
 auth(app);
 
 server = app.listen(getConfig(app).port, function() {
-    console.log('Listening on port %d', server.address().port);
+    console.log(require('../package').name + ' express app listening on port %d', server.address().port);
 });
--- app/get-config.js
+++ app/get-config.js
@@ -3,15 +3,15 @@
 var configMap = Object.freeze({
     production: {
         fitbitClientKey: process.env.FITBIT_DATA_EXPORT_CLIENT_KEY,
-            fitbitClientSecret: process.env.FITBIT_DATA_EXPORT_CLIENT_SECRET,
-            host: 'fitbit-export.azurewebsites.net',
-            port: process.env.PORT || 3001
+        fitbitClientSecret: process.env.FITBIT_DATA_EXPORT_CLIENT_SECRET,
+        host: 'fitbit-export.azurewebsites.net',
+        port: process.env.PORT || 3001
     },
     development: {
         fitbitClientKey: process.env.FITBIT_DATA_EXPORT_CLIENT_KEY,
-            fitbitClientSecret: process.env.FITBIT_DATA_EXPORT_CLIENT_SECRET,
-            host: 'localhost',
-            port: process.env.PORT || 3001
+        fitbitClientSecret: process.env.FITBIT_DATA_EXPORT_CLIENT_SECRET,
+        host: 'localhost',
+        port: process.env.PORT || 3001
     }
 });
 
