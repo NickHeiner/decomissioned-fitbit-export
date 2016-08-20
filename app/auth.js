efa08df35e3495a9a052f17005df5171753b40ce
Nick Heiner <nth23@cornell.edu>
2016-08-20 10:51:48 -0400

    getting through the auth flow

--- app/auth.js
+++ app/auth.js
@@ -3,14 +3,14 @@
 var passport = require('passport'),
     getConfig = require('./get-config'),
     url = require('url'),
-    FitbitOAuth2Strategy = require('passport-fitbit-oauth2').FitbitOAuth2Strategy,
-    callbackPath = '/auth/fitbit/callback';
+    FitbitOAuth2Strategy = require('passport-fitbit-oauth2').FitbitOAuth2Strategy;
 
 function auth(app) {
-    var config = getConfig(app),
-        callbackUrl;
+    var config = getConfig(app);
 
-    app.get('/auth/fitbit', passport.authenticate('fitbit', { scope: []}));
+    const callbackPath = '/auth/fitbit/callback';
+
+    app.get('/auth/fitbit', passport.authenticate('fitbit', { scope: ['activity', 'profile']}));
     app.get(
         callbackPath,
         passport.authenticate('fitbit', { 
@@ -27,7 +27,7 @@ function auth(app) {
         done(null, obj);
     });
 
-    callbackUrl = url.format({
+    const callbackUrl = url.format({
         protocol: 'http',
         host: config.host,
         port: config.port,
@@ -39,7 +39,7 @@ function auth(app) {
         clientSecret: config.fitbitClientSecret,
         callbackUrl: callbackUrl
     }, function(token, tokenSecret, profile, done) {
-        console.log('Logged in user ', profile.id, ' with display name ', profile.displayName);
+        console.log(`Logged in user ${profile.id} with display name ${profile.displayName}`);
 
         profile.token = token;
         profile.tokenSecret = tokenSecret;
